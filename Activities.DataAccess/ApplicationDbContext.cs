8654a8dda2bffe807d75ce42be5e4d74d6cca082
Ajey <ajey.pandiyaraj@cyberlegends.com>
2024-04-03 18:17:50 -0400

    [Initial commit]

---
+++ Activities.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Model;
+
+namespace DataAccess {
+	public class ApplicationDbContext : DbContext {
+		public DbSet<Activity> Activities { get; set; }
+	}
+}
+
